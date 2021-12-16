fb3365fe9048e86afdb50f36b101f7381a961aa8
C0dy98 <89564003+C0dy98@users.noreply.github.com>
2021-12-15 22:32:47 -0800

    Add files via upload

---
+++ mp.js
@@ -0,0 +1,11 @@
+function checkInput() {
+    var query = document.getElementById('search').value;
+    window.find(query);
+    return true;
+}
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+  }
+  function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+  }
